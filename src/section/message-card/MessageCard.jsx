5e9a3cc05947e6a4607f7c56682a998e0a49b1c8
simfrisk <simoncarlfrisk@gmail.com>
2025-04-29 16:22:39 +0200

    I styled the message card

--- src/section/message-card/MessageCard.jsx
+++ src/section/message-card/MessageCard.jsx
@@ -5,66 +5,75 @@ const MessageCardWrapper = styled.fieldset`
 display: flex;
 flex-direction: column;
 justify-content: center;
-background-color: #f5f5f5;
+background-color: #eeeeee;
 border: 1px solid black;
+border-radius: 2px;
 width: 320px;
 margin: 0 auto;
-padding: 8px;
-gap: 10px;
+padding: 12px 18px;
+gap: 12px;
+box-shadow: 10px 10px 0 rgba(0, 0, 0, 1);
 
 input {
 height: 40px;
-width: 90%;
+width: 100%;
+padding: 10px 20px;
 }
 
 button {
   background-color: pink;
   border-radius: 50px;
-  width: 65%;
+  width: 75%;
   border: none;
-  padding: 10px;
+  padding: 12px;
+  margin: 12px 0;
 
   &:hover {
-  background-color: red;
-}
+  background-color: #f0aeae;
+  }
 }
 
+@media (min-width: 640px) {
+width: 620px;
 
+}
 `
 
-export const MessageData = () => {
+// export const MessageData = () => {
 
-  let message = [
-    {
-      id: 0,
-      text: "",
-      timestamp: "",
-      likes: 0    
-    }
-  ]
-  
-}
+//   let message = [
+//     {
+//       id: 0,
+//       text: "",
+//       timestamp: "",
+//       likes: 0
+//     }
+//   ]
+
+// }
 
 export const MessageCard = () => {
 
-const [text, setText] = useState("")
+  const [text, setText] = useState("")
 
   return (
     <form onSubmit={(event) => event.preventDefault()}>
-    <MessageCardWrapper>
-        <legend>What´s making you happy right now?</legend>
+      <MessageCardWrapper>
+        <p>What´s making you happy right now?</p>
+        {/* <legend>What´s making you happy right now?</legend> */}
         <label>
-        <input 
-        type="text"
-        value={text}
-        onChange={(event) => setText(event.target.value)}
-        placeholder="Wright something happy!"
-        />
+          {/* What´s making you happy right now? */}
+          <input
+            type="text"
+            value={text}
+            onChange={(event) => setText(event.target.value)}
+            placeholder="Wright something happy!"
+          />
         </label>
-        <button type="submit">❤️ Send Happy Thoughts ❤️</button>        
-        </MessageCardWrapper>
-      
-    </form>
+        <button type="submit">❤️ Send Happy Thoughts ❤️</button>
+      </MessageCardWrapper>
+
+    </form >
 
 
   )
