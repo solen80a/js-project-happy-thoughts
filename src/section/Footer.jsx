1726d4ff4ef90bb299b30c1df28c2d2475f719be
solen80a <sofialennbom@gmail.com>
2025-05-02 12:12:18 +0200

    Made some styling to the footer and added a link to github.

--- src/section/Footer.jsx
+++ src/section/Footer.jsx
@@ -4,19 +4,31 @@ const FooterWrapper = styled.footer`
   display: flex;
   gap: 12px;
   align-items: center;
-  justify-content: center;
+  justify-content: left;
   font-size: 15px;
-  padding: 24px;
+  /* padding: 24px; */
   background-color: pink; 
   left: 0px;
   bottom: 0px; 
   right: 0px;
-  margin-bottom: 0px;
-  /* margin-top: 100px; */
+  margin-bottom: 0px; 
   position: fixed;
+  border: 1px solid #000;  
+  box-shadow: 10px 10px 0 rgba(0, 0, 0, 1);
+  width: 250px;
+  height: 50px;
+  margin: 28px auto; 
+  padding: 12px 18px;
+  gap: 12px;
+  transition: ease 1s; 
 
-  i {
-    font-size: 30px;
+  @media (min-width: 640px) {
+    width: 350px;    
+  }
+
+  img {
+    background-color: #FE8E86;
+    border-radius: 50%;   
   }
 
 `
@@ -25,6 +37,9 @@ export const Footer = () => {
   return (
     <FooterWrapper>
       <p>Created by: Simon & Sofia</p>
+      <a href="https://github.com/solen80a/js-project-happy-thoughts">
+        <img src="./assets/Btn - github.svg" alt="Github icon to access the code" target="_blank"/>
+      </a>
     </FooterWrapper>
   )
 }
