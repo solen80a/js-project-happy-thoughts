ebf0bdd94ff03f1057ae369aa7724233c152362c
simfrisk <simoncarlfrisk@gmail.com>
2025-05-06 20:58:21 +0200

    added H1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Cards } from "./section/cards/Cards";
+import { H1 } from "./styling/Typography"
 import { Footer } from "./section/Footer";
 import { MessageCardRecent } from "./section/message-card/MessageCardRecent";
 
@@ -6,8 +7,8 @@ export const App = () => {
 
   return (
     <>
-      <h1 style={{ textAlign: 'center' }}>Happy Thoughts</h1>
-      <Cards />      
+      <H1>Happy Thoughts</H1>
+      <Cards />
       <Footer />
     </>
   )
